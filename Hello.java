b2b8e3675c632a01081e15c3a483114f9550a292
Fortune <2513085850@qq.com>
2025-02-27 11:03:18 +0800

    提交测试

--- Hello.java
+++ Hello.java
@@ -4,7 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-
         System.out.println("Hello World");
         System.out.println(str);
     }
